a2a8e3150d3b7e31c05526e62c8402132a6e1945
Lori Moehn <lmoehn@gmail.com>
2013-09-16 12:18:55 -0700

    Holiday Table and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Holiday.create! name: "New Year's Day", holidate: "2013/01/01"
+Holiday.create! name: "Birthday of Martin Luther King, Jr.", holidate: "2013/01/21"
+Holiday.create! name: "Washington's Birthday", holidate: "2013/02/18"
+Holiday.create! name: "Memorial Day", holidate: "2013/05/27"
+Holiday.create! name: "Independence Day", holidate: "2013/07/04"
+Holiday.create! name: "Labor Day", holidate: "2013/09/02"
+Holiday.create! name: "Columbus Day", holidate: "2013/09/02"
+Holiday.create! name: "Vetern's Day", holidate: "2013/11/11"
+Holiday.create! name: "Thanksgiving", holidate: "2013/11/28"
+Holiday.create! name: "Christmas", holidate: "2013/12/25"
